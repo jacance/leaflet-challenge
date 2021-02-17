4dc4c7a85ea295e0a119510a924241efa957f7f6
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-02-16 21:55:10 -0800

    circlemarkers done, bindpopup done

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,8 +1,10 @@
+// Create our initial map object
 var myMap = L.map("mapid", {
-    center: [37.7749, -122.4194],
-    zoom: 13
+    center: [39.3210, -111.0937],
+    zoom: 6
   });
 
+// Add a tile layer to map (background map image)
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -12,37 +14,61 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
-
+// Url for analysis
 const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Function for returning colors based on depth
+function colorFunction(depth) {
+    if (depth > -10 && depth < 10) {
+        return "#00ff00"
+    }else if (depth >= 10 && depth < 30) {
+        return "#ccff66"
+    }else if (depth >= 30 && depth < 50) {
+        return "#ffcc66"
+    }else if (depth >= 50 && depth < 70) {
+        return "#ffcc00"
+    }else if (depth >= 70 && depth < 90) {
+        return "#ff9900"
+    }else {
+        return "#ff3300"}
+};
 
 
 d3.json(queryUrl, function(response) {
-    // console.log(response)
-    var feature = response.features
+    // Create entire geoJSON layer to be applied to map
+    L.geoJSON(response, {
 
-    // Empty arrays
-    var latLong = []
-    var depth = []
-    var magnitude = []
+        // Defines what to do with each point in JSON
+        pointToLayer: function (feature, latlng) {
 
-    // Iterate through results for earthquake information then push to empty array
-    feature.forEach(function(i) {
-        var coordinates = i.geometry.coordinates
-        latLong.push([coordinates[1], coordinates[0]]);
-        depth.push(coordinates[2])
-        var mag = i.properties.mag
-        magnitude.push(mag)
-    })
-    console.log(latLong)
-    console.log(depth)
-    console.log(magnitude)
+            var depth = feature.geometry.coordinates[2]
+            var mag = feature.properties.mag
 
-    // L.heatLayer(latLong, {
-    //     radius: 20,
-    //     blur: 35
-    
-    // }).addTo(myMap);
+            // Circle marker applied for each feature
+            return L.circleMarker(latlng, {
+                color: "black",
+
+                // Fill color based on depth with function
+                fillColor: colorFunction(depth),
+                fillOpacity: .9,
+                radius: mag * 4,
+                weight: 1,
+          })
+        },
+        // Function to click each feature for more pop up information
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<b>Place:</b> " + feature.properties.place + "<br><b>Magnitude:</b> " + feature.properties.mag +
+             "<br><b>Depth:</b> " + feature.geometry.coordinates[2]);
+          },
+
+        // 
+    // Add entire geoJSON layer to map
+    }).addTo(myMap);
+
 });
+
+
+   
 
 // Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
 
